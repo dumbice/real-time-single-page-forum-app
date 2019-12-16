c8f67417869c72ea9af8f1dabb2b35a92de5a32b
dumbice <gegle.bill@gmail.com>
2019-12-16 21:57:41 +0900

    Vuetify, Vue Router and Initial Components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-title>Y-knou</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn text>Forum</v-btn>
+            <v-btn text>Ask Question</v-btn>
+            <v-btn text>Category</v-btn>
+            <v-btn text>Login</v-btn>
+        </v-toolbar-items>
+
+        <template v-if="$vuetify.breakpoint.smAndUp">
+<!--            <v-btn icon>-->
+<!--                <v-icon>mdi-export-variant</v-icon>-->
+<!--            </v-btn>-->
+<!--            <v-btn icon>-->
+<!--                <v-icon>mdi-delete-circle</v-icon>-->
+<!--            </v-btn>-->
+<!--            <v-btn icon>-->
+<!--                <v-icon>mdi-plus-circle</v-icon>-->
+<!--            </v-btn>-->
+        </template>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
